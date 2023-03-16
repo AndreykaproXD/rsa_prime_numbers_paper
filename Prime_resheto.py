1d9900dbc41ffefb015c6a95ecf14129d6565f66
Andrey Kostrigin <andrey.n.kostr@gmail.com>
2023-03-16 22:46:24 +0300

    Prime_resheto.py: rename RSA algorithm variables and fix calculations

--- Prime_resheto.py
+++ Prime_resheto.py
@@ -18,19 +18,25 @@ def prime(N):
     return n
 
 
-def generator_e(p,q):
+def generator_e(p,q,f_n):
     e=random.choice(primes)
-    while e>=p or e>=q:
+    while e>=f_n or e==p or e==q:
         e=random.choice(primes)
-    print(e)
+        print("e=",e)
     return e
 
 
-def generator_d(p,q,e):
-    d=random.choice(primes)
-    while (e*d)%f_n!=1:
-        d=random.choice(primes)
-    print(d)
+def generator_d(e,f_n):
+    c=1
+    while 1:
+        d=c*round(f_n/e)+1
+        c+=1
+        mod_div_rem=(d*e)%f_n
+        print("d=",d,"; mod_div_rem=", mod_div_rem)
+        if mod_div_rem==1:
+            break
+        #if c>20:
+         #   break
     return d
 
 def alphabet():
@@ -39,14 +45,14 @@ def alphabet():
         rus.update({chr(i+1039):i})
     return (rus)
 
-def code(rus):
+def code(rus,e,n):
     i = str(input("Введите русскую заглавную букву:"))
     x=rus.get (i)
-    print(x)
+    print("x=",x)
     y=(x**e)%n
     return y
 
-def decode(y):
+def decode(y,d,n):
     z=(y**d)%n
     return z
 
@@ -55,22 +61,28 @@ def decode(y):
 primes = []
 primes = prime(int(input("Введите диапазон простых чисел:")))
 print(primes)
-p=random.choice(primes)
-q=random.choice(primes)
-while p==q:
-    q=random.choice(primes)
-print("p=",p)
-print("q=",q)
-n = p*q
-print("n=",n)
-f_n=(p-1)*(q-1)
-print("f(n)=",f_n)
-e=generator_e(p,q)
-d=generator_d(p,q,e)
-print("e=",e)
-print("d=",d)
+rsa_p=random.choice(primes)
+rsa_q=random.choice(primes)
+#rsa_p=23
+#rsa_q=37
+while rsa_p==rsa_q:
+    rsa_q=random.choice(primes)
+rsa_n = rsa_p*rsa_q
+rsa_f_n=(rsa_p-1)*(rsa_q-1)
+rsa_e=generator_e(rsa_p,rsa_q,rsa_f_n)
+#rsa_e=7
+print("p=",rsa_p)
+print("q=",rsa_q)
+print("n=",rsa_n)
+print("f(n)=",rsa_f_n)
+print("e=",rsa_e)
+rsa_d=generator_d(rsa_e,rsa_f_n)
+if rsa_e==rsa_d:
+    print("Ошибка: публичный и приватный ключи равны")
+
+print("d=",rsa_d)
 abc=alphabet()
-coded_num=code(abc)
-decoded_num=decode(coded_num)
+coded_num=code(abc,rsa_e,rsa_n)
+decoded_num=decode(coded_num,rsa_d,rsa_n)
 print("Закодированное число -",coded_num)
 print("Расшифрованное число -",decoded_num)
